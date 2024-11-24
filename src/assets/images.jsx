65d6844389f0a3da2eb4c5a3ba90255601e85fcd
Jamyle Elen <jamyleelen09@gmail.com>
2024-11-24 13:47:49 -0300

    feat: added new icons and applied responsiveness

--- src/assets/images.jsx
+++ src/assets/images.jsx
@@ -1,7 +1,17 @@
 import Logo from './images/logo.webp'
+import Car from './images/car.png'
+import Coffee from './images/coffee.png'
+import Restaurant from './images/restaurant.png'
+import Wifi from './images/wifi.png'
+import Location from './images/location.png'
 
 const Images = {
-    Logo
+    Logo,
+    Car,
+    Coffee,
+    Restaurant,
+    Wifi,
+    Location
 }
 
 export default Images
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,35 +2,44 @@ import styled from "styled-components"
 import logoOnfly from "../../assets/img/logoOnfly.png"
 import { Link } from "react-router-dom";
 
+const CabecalhoContainer = styled.header`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`
+
 const Cabecalho = styled.header`
+    max-width: 1200px;
+    width: 100%;
     height: 70px;
+    /* border: red solid; */
     background-color: white;
     display: flex;
     align-items: center;
     justify-content: space-between;
     padding: 0 20px;
-    
 `
 
 const Moldura = styled.div`
-    widht: 55px;
-    height: 55px;
+    width: 90px;
 `
 
 const Logo = styled.img`
-    widht: 100%;
+    width: 100%;
     height: 100%;
 `
 
-const Div = styled.div`
+const Div = styled.nav`
     display: flex;
-    gap: 10px
+    gap: 8px;
+    /* border: red solid 1px; */
+    align-items: center;
 `
 
 const ButtonWhite = styled.button`
     color: #0597FA;
-    widht: 50px;
-    height: 50px;
+    /* border: red solid 1px; */
+    width: 100px;
     font-family: var(--font-family);
     font-size: 16px;
     padding: 5px;
@@ -43,8 +52,8 @@ const ButtonWhite = styled.button`
 const ButtonBlue = styled.button`
     cursor: pointer;
     color: white;
-    widht: 50px;
-    height: 50px;
+    /* border: red solid 1px; */
+    width: 140px;
     font-family: var(--font-family);
     font-size: 16px;
     padding: 10px;
@@ -52,28 +61,34 @@ const ButtonBlue = styled.button`
     font-weight: 600;
     background-color: #0597FA;
     border-radius: 10px;
-`
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    transition: all .4s ease-in-out;
 
+    &:hover {
+        background-color: #33a2ed;
+    }
+`
 
 export const Header = () => {
     return(
-        <Cabecalho>
-            <Link to={"/"}>
-                <Moldura>
-                    <Logo src={logoOnfly} alt="Logo Onfly"/>
-                </Moldura>
-            </Link>
+        <CabecalhoContainer>
             
-            <Div>
-                <Link to={"/loginUser"}>
-                    <ButtonWhite>Entrar</ButtonWhite> 
+            <Cabecalho>
+                <Link to={"/"}>
+                    <Moldura>
+                        <Logo src={logoOnfly} alt="Logo Onfly"/>
+                    </Moldura>
                 </Link>
-
-                <Link to={"/registerUser"}>
-                    <ButtonBlue>Cadastre-se</ButtonBlue>               
-                </Link>
-
-            </Div>
-        </Cabecalho>
+            
+                <Div>
+                    <Link to={"/loginUser"}>
+                        <ButtonWhite>Entrar</ButtonWhite>
+                    </Link>
+                    <Link to={"/registerUser"}>
+                        <ButtonBlue>Cadastre-se</ButtonBlue>
+                    </Link>
+                </Div>
+            </Cabecalho>
+        </CabecalhoContainer>
     )
 }
