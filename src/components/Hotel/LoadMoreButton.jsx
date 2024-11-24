a8920e31c142a0987159815dff3b6788e7645f59
kennedfer <kenned14santos@gmail.com>
2024-11-24 16:21:41 -0300

    feat: Add style to "LoadMore" button

--- src/components/Hotel/LoadMoreButton.jsx
+++ src/components/Hotel/LoadMoreButton.jsx
@@ -2,6 +2,11 @@ import styled from "styled-components";
 
 const LoadMoreButtonStyled = styled.button`
   padding: 20px;
+  background-color: var(--card-button-background);
+  color: var(--card-button-foreground);
+
+  border: none;
+  border-radius: 10px;
 `;
 
 export function LoadMoreButton({ onclick }) {
