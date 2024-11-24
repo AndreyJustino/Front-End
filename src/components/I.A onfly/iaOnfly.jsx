9c3f11d85e5fc7713d7452276f5ff4257e2f44c5
kennedfer <kenned14santos@gmail.com>
2024-11-24 19:30:50 -0300

    fix: Fix wrong ai button position

--- src/components/I.A onfly/iaOnfly.jsx
+++ src/components/I.A onfly/iaOnfly.jsx
@@ -1,33 +1,37 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import SpeedDial from '@mui/material/SpeedDial';
-import SpeedDialIcon from '@mui/material/SpeedDialIcon';
-import RecipeReviewCard from './cardIaOnfly'; 
+import * as React from "react";
+import Box from "@mui/material/Box";
+import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
+import RecipeReviewCard from "./cardIaOnfly";
 
 export default function BasicSpeedDial() {
   const [showCard, setShowCard] = React.useState(false);
 
   const handleToggle = (e) => {
-    e.stopPropagation(); 
+    e.stopPropagation();
     setShowCard((prevShowCard) => !prevShowCard);
   };
 
   return (
-    <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 }}>
+    <Box sx={{}}>
       <SpeedDial
         ariaLabel="SpeedDial basic example"
-        sx={{ position: 'absolute', bottom: 800, right: -100 }}
+        sx={{
+          position: "fixed",
+          bottom: 20,
+          right: 20,
+        }}
         icon={<SpeedDialIcon />}
         onClick={handleToggle}
       />
       {showCard && (
         <Box
           onClick={(e) => e.stopPropagation()}
-          sx={{ position: 'absolute', bottom: 900, right: -100 }}
+          sx={{ position: "fixed", bottom: 100, right: 20 }}
         >
           <RecipeReviewCard />
         </Box>
       )}
     </Box>
-  );  
+  );
 }
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home }  from "../pages/home/home.jsx";
+import { Home } from "../pages/home/home.jsx";
 // import { Footer } from "../components/footer/footer.jsx";
 import { LoginUser } from "../pages/loginPages/loginUser.jsx";
 import { LoginCompany } from "../pages/loginPages/loginCompany.jsx";
 import { RegisterCompany } from "../pages/registerPages/registerCompany.jsx";
 import { RegisterUser } from "../pages/registerPages/registerUser.jsx";
 import { AboutHosting } from "../pages/aboutHosting/aboutHosting.jsx";
+import BasicSpeedDial from "../components/I.A onfly/iaOnfly.jsx";
 
 export const AppRoutes = () => {
   return (
@@ -14,14 +15,12 @@ export const AppRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/loginUser" element={<LoginUser />} />
         <Route path="/loginCompany" element={<LoginCompany />} />
-        <Route path="/registerCompany" element={<RegisterCompany/>} />
-        <Route path="/registerUser" element={<RegisterUser/>} />
-        <Route path="/aboutHosting" element={<AboutHosting/>} />
+        <Route path="/registerCompany" element={<RegisterCompany />} />
+        <Route path="/registerUser" element={<RegisterUser />} />
+        <Route path="/aboutHosting" element={<AboutHosting />} />
       </Routes>
       {/* <Footer />  */}
+      <BasicSpeedDial />
     </BrowserRouter>
   );
 };
-
-
-
