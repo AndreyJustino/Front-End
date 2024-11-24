6ceeb6765d2e500093d70d629f1d29aa23cb203f
AndreyJustino <andreyjustino2020@gmail.com>
2024-11-24 03:22:25 -0300

    feat: create second header

---
+++ src/pages/aboutHosting/aboutHosting.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+import { Header } from "../../components/header/header"
+
+
+export const AboutHosting = () => {
+    return (
+        <>
+            <Header></Header>
+        </>
+    )
+}
