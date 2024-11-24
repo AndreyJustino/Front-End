faa035344030306a7593b3e4e22be027eef9028f
AndreyJustino <andreyjustino2020@gmail.com>
2024-11-23 22:01:52 -0300

    feat: add basic reset

--- src/pages/loginUser/loginUser.jsx
+++ src/pages/loginUser/loginUser.jsx
@@ -3,16 +3,16 @@ import { Button } from '../../components/button/button'
 import { Input } from '../../components/input/input'
 import styled from 'styled-components'
 
-const Div = styled.div`
-    width: 100px
+const Main = styled.main`
+    width: 100vw;
+    height: 100vh;
+    border: 2px solid red
 `
 
 export const LoginUser = (props) => {
   return (
     <>
-        <Div>
-            <Input></Input>
-        </Div>
+        <Main></Main>
     </>
   )
 }
